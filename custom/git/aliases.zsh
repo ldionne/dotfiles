dc75bbd64cbf2e64178b196ddfc72b663ce46847
Louis Dionne <ldionne.2@gmail.com>
2013-10-04 00:02:06 -0400

    Show diff when writing git commits.

--- custom/git/aliases.zsh
+++ custom/git/aliases.zsh
@@ -8,8 +8,8 @@ alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -v'
+alias gca='git commit -va'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
