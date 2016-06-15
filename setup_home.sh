60bfa119845681887a0c92997654a05bc8ed1060
Louis Dionne <ldionne.2@gmail.com>
2016-06-14 21:54:15 -0700

    Make setup_home idempotent

--- setup_home.sh
+++ setup_home.sh
@@ -3,7 +3,7 @@
 ##############################################################################
 # Link each file or directory in the `symlinks` directory into `$HOME`.
 ##############################################################################
-ln -s ${PWD}/symlinks/.bin ${HOME}/.bin
+[ -d ${HOME}/.bin ] || ln -s ${PWD}/symlinks/.bin ${HOME}/.bin
 ln -s ${PWD}/symlinks/.gitconfig ${HOME}/.gitconfig
 ln -s ${PWD}/symlinks/.gitignore ${HOME}/.gitignore
 ln -s ${PWD}/symlinks/.iterm2.zsh ${HOME}/.iterm2.zsh
@@ -13,6 +13,7 @@ echo "symlinked dotfiles into ${HOME}"
 ##############################################################################
 # Link the `sublime3/User` directory to Sublime Text's User directory.
 ##############################################################################
-ln -s "${PWD}/sublime3/User" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
+SUBLIME_DATA_DIR="${HOME}/Library/Application Support/Sublime Text 3"
+[ -d "${SUBLIME_DATA_DIR}/Packages/User" ] || ln -s "${PWD}/sublime3/User" "${SUBLIME_DATA_DIR}/Packages/User"
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
 echo "preferences for Sublime Text 3 set"
