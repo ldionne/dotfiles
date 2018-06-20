828053b86ec0a7850a7595ec71151eddd403848f
Louis Dionne <ldionne.2@gmail.com>
2018-06-20 13:03:20 -0700

    Rework how we symlink Sublime preferences

--- setup_home.sh
+++ setup_home.sh
@@ -2,18 +2,37 @@
 
 set -e
 
+DOTFILES_ROOT="${PWD}"
+
 ##############################################################################
 # Link each file or directory in the `symlinks` directory into `$HOME`.
 ##############################################################################
-[ -d ${HOME}/.bin ] || ln -s ${PWD}/symlinks/.bin ${HOME}/.bin
-for file in .fzf.zsh .gitconfig .gitignore .iterm2.zsh .zshrc; do
-  [[ -L ${HOME}/${file} ]] || ln -s ${PWD}/symlinks/${file} ${HOME}/${file}
+echo "***********************************************"
+echo "* symlinking dotfiles into ${HOME}"
+echo "***********************************************"
+
+for file in .fzf.zsh .gitconfig .gitignore .zshrc .bin; do
+  dest="${HOME}/${file}"
+  source="${DOTFILES_ROOT}/symlinks/${file}"
+  if [[ ! -L "${dest}" ]]; then
+    ln -s "${source}" "${dest}"
+    echo "symlinked ${dest} to ${source}"
+  fi
 done
-echo "symlinked dotfiles into ${HOME}"
 
 ##############################################################################
-# Link the `sublime3/User` directory to Sublime Text's User directory.
+# Link the Sublime package preferences in the sublime3 directory to the
+# Sublime Text directory that contains those settings.
 ##############################################################################
+echo "***********************************************"
+echo "* symlinking Sublime Text 3 preferences"
+echo "***********************************************"
+
 SUBLIME_DATA_DIR="${HOME}/Library/Application Support/Sublime Text 3"
-[ -d "${SUBLIME_DATA_DIR}/Packages/User" ] || ln -s "${PWD}/sublime3/User" "${SUBLIME_DATA_DIR}/Packages/User"
-echo "preferences for Sublime Text 3 set"
+for file in "${DOTFILES_ROOT}/sublime3"/*; do
+  dest="${SUBLIME_DATA_DIR}/Packages/User/$(basename ${file})"
+  if [[ ! -L "${dest}" ]]; then
+    ln -s "${file}" "${dest}"
+    echo "symlinked Sublime Text preferences ${dest} to ${file}"
+  fi
+done
