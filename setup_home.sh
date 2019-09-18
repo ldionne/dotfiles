871d77ee1a3e869082049afb53dc411f7b81f446
Louis Dionne <ldionne.2@gmail.com>
2019-09-18 15:54:15 -0600

    Allow setting up Sublime Text separately

--- setup_home.sh
+++ setup_home.sh
@@ -19,20 +19,3 @@ for file in .fzf.zsh .gitconfig .gitconfig-work .gitignore .zshrc .bin .config; 
     echo "symlinked ${dest} to ${source}"
   fi
 done
-
-##############################################################################
-# Link the Sublime package preferences in the sublime3 directory to the
-# Sublime Text directory that contains those settings.
-##############################################################################
-echo "***********************************************"
-echo "* symlinking Sublime Text 3 preferences"
-echo "***********************************************"
-
-SUBLIME_DATA_DIR="${HOME}/Library/Application Support/Sublime Text 3"
-for file in "${DOTFILES_ROOT}/sublime3"/*; do
-  dest="${SUBLIME_DATA_DIR}/Packages/User/$(basename "${file}")"
-  if [[ ! -L "${dest}" ]]; then
-    ln -s "${file}" "${dest}"
-    echo "symlinked Sublime Text preferences ${dest} to ${file}"
-  fi
-done
