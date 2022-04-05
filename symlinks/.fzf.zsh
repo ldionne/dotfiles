36d8b69782d475290e2628c052376d74a35e547c
Louis Dionne <ldionne.2@gmail.com>
2022-04-05 11:20:06 -0400

    Adjust fzf for arm64 macs

--- symlinks/.fzf.zsh
+++ symlinks/.fzf.zsh
@@ -1,16 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *$(brew --prefix)/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$(brew --prefix)/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-local key_bindings="/usr/local/opt/fzf/shell/key-bindings.zsh"
-if [[ -f "${key_bindings}" ]]; then
-    source "${key_bindings}"
-fi
+source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
